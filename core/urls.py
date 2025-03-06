aaa4d7425d65cfc7684c036dce39bb83ce8a3f7e
sagar sangwan <iamsagarsangwan@gmail.com>
2025-03-06 19:21:31 +0530

    2nd taska completed

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import google_login
+from .views import google_login, google_callback
 
-urlpatterns = [path("api/auth/login", google_login, name="google_login")]
+urlpatterns = [
+    path("api/auth/login", google_login, name="google_login"),
+    path("api/auth/callback/", google_callback, name="google_callback"),
+]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 import os
 from django.http import JsonResponse
 from urllib.parse import urlencode
+import requests
 
 
 def google_login(request):
@@ -15,3 +16,30 @@ def google_login(request):
     print(os.environ.get("GOOGLE_REDIRECT_URI"))
     auth_url = f"{google_auth_url}?{urlencode(params)}"
     return JsonResponse({"auth_url": auth_url})
+
+
+def google_callback(request):
+    code = request.GET.get("code")
+    print(code)
+    if not code:
+        return JsonResponse({"error": "no code provided"}, status=400)
+    token_url = "https://oauth2.googleapis.com/token"
+    token_data = {
+        "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+        "client_secret": os.environ.get("GOOGLE_CLIENT_SECRET"),
+        "code": code,
+        "redirect_uri": os.environ.get("GOOGLE_REDIRECT_URI"),
+        "grant_type": "authorization_code",
+    }
+    token_response = requests.post(token_url, data=token_data)
+    token_json = token_response.json()
+    if "access_token" not in token_json:
+        return JsonResponse({"error": "failed to get access token"}, error=500)
+    access_token = token_json["access_token"]
+    user_info_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    user_info_response = requests.get(
+        user_info_url, headers={"Authorization": f"Bearer {access_token}"}
+    )
+    user_info = user_info_response.json()
+    user_info["access_token"] = access_token
+    return JsonResponse(user_info)
