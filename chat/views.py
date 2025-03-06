7672b865133497898fc0751feb4bf7d267c7fbe3
sagar sangwan <iamsagarsangwan@gmail.com>
2025-03-07 00:45:46 +0530

    updated code

--- chat/views.py
+++ chat/views.py
@@ -6,4 +6,5 @@ import os
 
 def chat_view(request):
     websocket_url = os.environ.get("WEBSOCKET_URL")
+    print(websocket_url)
     return render(request, "index.html", {"websocket_url": websocket_url})
