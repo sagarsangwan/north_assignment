c606063507b262123ed5df89540bbbda5ddccb03
sagar sangwan <iamsagarsangwan@gmail.com>
2025-03-06 22:24:03 +0530

    chat working fimr

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,18 +4,23 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsuer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_group_name = "chatroom"
-        self.group_name = f"chat_{self.room_group_name}"
+        print(self)
+        self.room_name = "chatroom"
+        self.room_group_name = f"chat_{self.room_name}"
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
+        print(self.channel_name)
+        print("WebSocket Connected")
 
-    async def disconnect(self):
+    async def disconnect(self, close_code):
+        print("disconnected")
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data=None):
         data = json.loads(text_data)
         message = data["message"]
         username = data["username"]
+        print(f"Received message from {username}: {message}")
         await self.channel_layer.group_send(
             self.room_group_name,
             {
